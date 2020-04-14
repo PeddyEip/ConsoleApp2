02e24ec30810dea2fee46f5dacbe24f1e3ed6d09
myswooop <myswooop@LAPTOP-81FUBD66>
2020-04-14 16:29:56 +0200

    Test

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,9 +4,12 @@ namespace ConsoleApp1
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello");
+            Console.WriteLine("Hello");
         }
     }
 }
